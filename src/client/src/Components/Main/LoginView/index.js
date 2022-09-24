09022087b55ef3ee81d56c1cea9e910fb919697b
Matthew Bicknell <64238751+Mbickne1@users.noreply.github.com>
2022-09-24 19:31:42 -0400

    Added simple login for user and guest

--- src/client/src/Components/Main/LoginView/index.js
+++ src/client/src/Components/Main/LoginView/index.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { TextField, Button } from '@mui/material';
 import styles from './LoginView.module.css';
+import useLoginService from './useLoginService';
 
 const LoginView = (props) => {
     const { setShowLogin } = props;
+    const [isValidUser, validate, validateGuest] = useLoginService();
 
-    const handleClick = () => {
-        console.log("Validate User Credentials...");
-        console.log("Load User Information...");
-        console.log("Switch To Shop View...");
-        setShowLogin(false);
+    useEffect(() => {
+        if(isValidUser) {
+            setShowLogin(false);
+        }
+    }, [isValidUser])
+
+    const attemptLogin = (event) => {
+        const target = event.target;
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        validate(username, password);
+    }
+
+
+
+    const guestLogin = (event) => {
+        const target = event.target;
+
+        validateGuest();
     }
 
     return (
         <div className={styles.container}>
             <div className={styles.fieldContainer}>
-                <TextField variant='filled' style={{width: '80%'}} label='Username'></TextField>
-                <TextField variant='filled' style={{width: '80%'}} label='Password'></TextField>
+                <TextField id='username' variant='filled' style={{width: '80%'}} label='Username'></TextField>
+                <TextField id='password' variant='filled' style={{width: '80%'}} label='Password'></TextField>
             </div>
             <div className={styles.buttonContainer}>
-                <Button onClick={handleClick} variant='contained' className={styles.login}>
+                <Button onClick={attemptLogin} variant='contained' className={styles.login}>
                     Log In
                 </Button>
             </div>
+            <Button variant='text' onClick={guestLogin}>Continue as Guest</Button>
         </div>
     );
 }
