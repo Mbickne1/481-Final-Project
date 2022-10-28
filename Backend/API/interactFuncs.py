8568ea775dd66e574a8515daea8ac4c994f1ad01
infinumtech <alyeldinshahin@gmail.com>
2022-10-27 21:42:33 -0400

    API Update with Working Function
    #TODO, Deploy to Deta

--- Backend/API/interactFuncs.py
+++ Backend/API/interactFuncs.py
@@ -1,47 +1,87 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    user='admin', password='Evett123',
-    host='final481.c9debhe4folh.us-east-2.rds.amazonaws.com',
-    database='shopping_cart')
+# mydb = mysql.connector.connect(
+#     user='admin', password='Evett123',
+#     host='final481.c9debhe4folh.us-east-2.rds.amazonaws.com',
+#     database='shopping_cart')
 
-mycursor = mydb.cursor()
+# mycursor = mydb.cursor()
 
-def deleteProduct (mycursor,item_name):
-    prepStatement = ("DELETE FROM productTable WHERE name = "+str(item_name))
+def deleteProduct (mydb,item_name):
+    mycursor = mydb.cursor()
+    prepStatement = ('DELETE FROM productTable WHERE name = "'+str(item_name)+'"')
     print(prepStatement)
     mycursor.execute(prepStatement)
     mydb.commit()
 
 
-def addItem(mycursor,item_name, price, brand):
+def addItem(mydb,item_name, price, brand):
+    mycursor = mydb.cursor()
     prepareString = "INSERT INTO productTable(name, price, brand) VALUES(%s,%s,%s)"
     val = (str(item_name), price, str(brand))
     mycursor.execute(prepareString, val)
     mydb.commit()
 
 
-def addUser(mycursor,userName, passWord):
+def addUser(mydb,userName, passWord):
+    mycursor = mydb.cursor()
     prepareString = "INSERT INTO userPass(userName, passWord) VALUES(%s,%s)"
     val = (str(userName),str(passWord))
     mycursor.execute(prepareString, val)
     mydb.commit()
 
 
-def deleteUser(mycursor,userName):
-    prepStatement = ("DELETE FROM userPass WHERE userName = "+str(userName))
+def deleteUser(mydb,userName):
+    mycursor = mydb.cursor()
+    prepStatement = ('DELETE FROM userPass WHERE userName = "'+str(userName)+'"')
     mycursor.execute(prepStatement)
     mydb.commit()
 
-
-def printItemList(mycursor):
+def printItemList(mydb):
+    mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM shopping_cart.productTable;")
     result = mycursor.fetchall()
     return result
 
+def addToCart(mydb, userName,itemName,price,brand):
+    mycursor = mydb.cursor()
+    prepareString = "INSERT INTO cart(userName,itemName,price,brand) VALUES(%s,%s,%s,%s)"
+    val = (str(userName),str(itemName),price, str(brand))
+    mycursor.execute(prepareString, val)
+    mydb.commit()
+
+def deleteFromCart(mydb, userName, itemName):
+    mycursor = mydb.cursor()
+    prepareString = 'DELETE FROM cart WHERE userName = "'+userName+'" AND itemName = "'+itemName+'"'
+    print(prepareString)
+    val = (str(itemName),str(userName))
+    mycursor.execute(prepareString)
+    returnText = 'number of rows deleted ' + str(mycursor.rowcount)
+    mydb.commit()
+    return(returnText)
+
+def clearCart(mydb, userName):
+    mycursor = mydb.cursor()
+    prepareString = 'DELETE FROM cart WHERE userName = "' +userName+'"'
+    print(prepareString)
+    mycursor.execute(prepareString)
+    returnText = 'number of rows deleted ' + str(mycursor.rowcount)
+    mydb.commit()
+    return (returnText)
+
+def getCart(mydb, userName):
+    mycursor = mydb.cursor()
+    mycursor.execute('SELECT * FROM shopping_cart.cart WHERE userName = "'+userName+'"')
+    result = mycursor.fetchall()
+    return result
 
 if __name__ == "__main__":
-    print("Starting")
-    mycursor.execute("SELECT * FROM shopping_cart.productTable;")
+    mydb = mysql.connector.connect(
+        user='admin', password='Evett123',
+        host='final481.c9debhe4folh.us-east-2.rds.amazonaws.com',
+        database='shopping_cart')
+
+    mycursor = mydb.cursor()
+    mycursor.execute("DROP TABLE `shopping_cart`.`cart`")
     result = mycursor.fetchall()
     print(result)
--- Backend/API/main.py
+++ Backend/API/main.py
@@ -1,9 +1,17 @@
+import imp
+from typing import ItemsView
 import uvicorn
+from interactFuncs import *
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import mysql.connector
 
+mydb = mysql.connector.connect(
+    user='admin', password='Evett123',
+    host='final481.c9debhe4folh.us-east-2.rds.amazonaws.com',
+    database='shopping_cart')
 
+mycursor = mydb.cursor()
 
 mycursor = "temp"
 
@@ -32,45 +40,58 @@ async def read_item():
     return ("Sucsuess")
 
 
-@app.post("/deleteItem")
-async def read_item(itemName: str):
-    return ("Sucsuess")
+@app.get("/getItems")
+async def read_item():
+    return (printItemList(mydb))
 
+@app.post("/deleteItem")
+async def read_item(item_name: str):
+    deleteProduct(mydb,item_name)
+    return ("Item "+item_name+" Deleted")
 
 @app.post("/addItem")
-async def read_item(itemName: str):
-    return ("Sucsuess")
+async def read_item(item_name: str,price: float,brand):
+    addItem(mydb,item_name,price,brand)
+    return ("Item "+item_name+" added")
 
 
 @app.post("/addUser")
 async def read_item(userName: str, passWord: str):
-    return ("Sucsuess")
+    addUser(mydb,userName,passWord)
+    return ("Added "+userName)
 
 
 @app.post("/deleteUser")
 async def read_item(userName: str):
-    return ("Sucsuess")
+    deleteUser(mydb, userName)
+    return ("Removed User "+userName)
 
 
-@app.get("/getItems")
-async def read_item():
-    return ("Sucsuess")
+
 
 
 @app.post("/addToCart")
-async def read_item(item_name: str):
-    return ("Sucsuess")
+async def read_item(user_name: str,item_name: str,price: float,brand: str):
+    addToCart(mydb,user_name,item_name,price,brand)
+    return ("Added to users Cart")
 
 
 @app.post("/deleteFromCart")
-async def read_item(item_name: str):
-    return ("Sucsuess")
+async def read_item(user_name: str,item_name:str):
+    response =deleteFromCart(mydb,user_name,item_name)
+    return (response)
 
 
 @app.post("/getCart")
 async def read_item(item_name: str):
-    return ("Sucsuess")
+    response = getCart(mydb,item_name)
+    return (response)
 
+
+@app.post("/clearCart")
+async def read_item(user_name: str):
+    response = clearCart(mydb,user_name)
+    return (response)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
